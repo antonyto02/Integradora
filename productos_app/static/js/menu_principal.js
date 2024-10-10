42c4e266fd76c2739a4055c8c52acc2c2d4fd5f8
Jose0305ja <arr2005ja@gmail.com>
2024-10-10 16:25:44 -0600

    1

---
+++ productos_app/static/js/menu_principal.js
@@ -0,0 +1,6 @@
+// Si deseas añadir interactividad, como cambiar de vista
+document.querySelectorAll(".card").forEach((button) => {
+  button.addEventListener("click", function () {
+    alert("Cambiando de vista"); // O lo que necesites implementar
+  });
+});
