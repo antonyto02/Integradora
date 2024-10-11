4fb1919c76c389827e542da6b13b1bf86c69b744
Jose0305ja <arr2005ja@gmail.com>
2024-10-11 16:30:35 -0600

    Co-authored-by: idalygf1 <idalygf1@users.noreply.github.com>

--- productos_app/views.py
+++ productos_app/views.py
@@ -12,11 +12,11 @@ def submenus(request, categoria):
     if categoria == "productos":
         context = {
             "opciones": [
-                {"opcion": "Mostrar todo", "link": "https://i.postimg.cc/W4nM5tBk/seleccionar-todo-1.png"},
-                {"opcion": "Limpieza", "link": "https://i.postimg.cc/vHrWxFgS/productos.png"},
-                {"opcion": "Insumos", "link": "https://i.postimg.cc/cJ2t60Wj/comida.png"},
-                {"opcion": "Desechables", "link": "https://i.postimg.cc/qq2s49Hn/contenedor-de-comida.png"},
-                {"opcion": "Loza", "link": "https://i.postimg.cc/tg6nZJ56/vajilla.png"},
+                {"opcion": "MOSTRAR TODO", "link": "https://i.postimg.cc/W4nM5tBk/seleccionar-todo-1.png"},
+                {"opcion": "LIMPIEZA", "link": "https://i.postimg.cc/vHrWxFgS/productos.png"},
+                {"opcion": "INSUMOS", "link": "https://i.postimg.cc/qBcLyTyC/comida-1.png"},
+                {"opcion": "DESECHABLES", "link": "https://i.postimg.cc/qq2s49Hn/contenedor-de-comida.png"},
+                {"opcion": "LOZA", "link": "https://i.postimg.cc/tg6nZJ56/vajilla.png"},
 
             ]
         }
@@ -24,11 +24,11 @@ def submenus(request, categoria):
     elif categoria=="empleados":
         context = {
             "opciones": [
-                {"opcion": "Mostrar todo", "link": "https://i.postimg.cc/W4nM5tBk/seleccionar-todo-1.png"},
-                {"opcion": "Cocina", "link": "https://i.postimg.cc/6QnJcnb6/camarero.png"},
-                {"opcion": "Mesero", "link": "link3"},
+                {"opcion": "MOSTRAR TODO", "link": "https://i.postimg.cc/W4nM5tBk/seleccionar-todo-1.png"},
+                {"opcion": "COCINA", "link": "https://i.postimg.cc/6QnJcnb6/camarero.png"},
+                {"opcion": "MESERO", "link": "link3"},
                 {"opcion": "RH", "link": "link4"},
-                {"opcion": "Recepcionista", "link": "link5"},
+                {"opcion": "RECEPCIONISTA", "link": "link5"},
 
             ]
         }
