0bd88399204439a693e5500326cbfc5b5af6733d
Jose0305ja <arr2005ja@gmail.com>
2024-10-11 10:05:40 -0600

    icono

--- productos_app/static/js/login.js
+++ productos_app/static/js/login.js
@@ -16,5 +16,3 @@ togglePassword.addEventListener("click", function (e) {
       : "M10 3C6 3 2.73 5.11 1.14 8.25a1 1 0 000 .77C2.73 12.89 6 15 10 15c4 0 7.27-2.11 8.86-5.25a1 1 0 000-.77C17.27 5.11 14 3 10 3zM10 13a3 3 0 110-6 3 3 0 010 6zm0-4a1 1 0 100 2 1 1 0 000-2z"
   );
 });
-
-// tailwind.config.js
