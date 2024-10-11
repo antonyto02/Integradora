bb5e2b027821364daa8674d0b8fa732b7f7eb961
antonyto02 <tonydelro@gmail.com>
2024-10-11 12:52:50 -0600

    Submenus listos

--- productos_app/views.py
+++ productos_app/views.py
@@ -6,13 +6,35 @@ def menu_principal(request):
     return render(request, 'productos_app/menu_principal.html')
 
 @login_required
-def productos(request):
-    return render(request, 'productos_app/productos.html')
+def submenus(request, categoria):
+    context = {}
+
+    if categoria == "productos":
+        context = {
+            "opciones": [
+                {"opcion": "Mostrar todo", "link": "link1"},
+                {"opcion": "Limpieza", "link": "link2"},
+                {"opcion": "Insumos", "link": "link3"},
+                {"opcion": "Desechables", "link": "link4"},
+                {"opcion": "Loza", "link": "link5"},
+
+            ]
+        }
+        
+    elif categoria=="empleados":
+        context = {
+            "opciones": [
+                {"opcion": "Mostrar todo", "link": "link1"},
+                {"opcion": "Cocina", "link": "link2"},
+                {"opcion": "Mesero", "link": "link3"},
+                {"opcion": "RH", "link": "link4"},
+                {"opcion": "Recepcionista", "link": "link5"},
+
+            ]
+        }
+        
+    return render(request, 'productos_app/submenus.html', context)
 
 @login_required
 def proveedores(request):
-    return render(request, 'productos_app/proveedores.html')
-
-@login_required
-def empleados(request):
-    return render(request, 'productos_app/empleados.html')
+    pass
